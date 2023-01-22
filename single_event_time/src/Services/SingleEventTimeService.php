29d8497707a855b0d786e1704a892cd90cb4020e
Matej <110779759+MatejP92@users.noreply.github.com>
2023-01-22 18:02:47 +0100

    switched timestamps with DateTime methods

--- single_event_time/src/Services/SingleEventTimeService.php
+++ single_event_time/src/Services/SingleEventTimeService.php
@@ -9,6 +9,7 @@ use Drupal\Core\Datetime\DrupalDateTime;
 
 class SingleEventTimeService{
   public function getEventTime($node = null){
+
     if(!($node instanceof NodeInterface) || $node->getType() != 'event') { return []; }
     $current_time = new DateTime();
     $timezone = new DateTimeZone(Drupal::config('system.date')->get('timezone')['default']);
@@ -16,15 +17,20 @@ class SingleEventTimeService{
     if($node->field_date){
       $event_date = new DrupalDateTime($node->field_date->value, $timezone);
       $current_time->setTimezone($timezone);
-      $time_to_event = round(($event_date->getTimestamp() - $current_time->getTimestamp()) / 86400);
-      if ($time_to_event > 1) {
-        $time_message = $time_to_event . ' ' . t('days left until event starts');
-      } elseif ($time_to_event == 0) {
-        $time_message = t('This event is happening today');
-      } elseif ($time_to_event == 1) {
-        $time_message = $time_to_event . ' ' . t('day left until event starts');
-      } else {
+      if($event_date < $current_time) {
         $time_message = t('Event has ended');
+      } else {
+        $time_diff = $event_date->diff($current_time);
+        $time_diff_hours = $time_diff->days * 24 + $time_diff->h;
+        if ($time_diff_hours < 24) {
+          if($event_date->format('Y-m-d') == $current_time->modify('+1 day')->format('Y-m-d')) {
+            $time_message = t('1 day left until event starts');
+          } else {
+            $time_message = t('Event is happening today');
+          }
+        } else {
+          $time_message = t('%days days left until event starts', array('%days' => $time_diff->format('%a')));
+        }
       }
       $event = [
         'time_message' => $time_message
@@ -33,3 +39,4 @@ class SingleEventTimeService{
     return $event;
   }
 }
+
